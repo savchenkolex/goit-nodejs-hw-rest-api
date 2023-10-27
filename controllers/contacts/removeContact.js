9fb2d426241fa90f765ee8f4703171aef68cb19f
lex <savchenko.lex@gmail.com>
2023-10-27 19:54:07 +0300

    add owner filter to removeContact query

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -2,10 +2,10 @@ const Contact = require("../../models/contactSchema.js");
 
 
 const removeContact = async (req, res, next) => {
-    
+      const currentUserID = req.User._id;
       const { contactId } = req.params;
   
-      const isRemoved = await Contact.findByIdAndRemove(contactId);
+      const isRemoved = await Contact.findOneAndRemove({_id: contactId, owner: currentUserID});
   
       if (isRemoved) {
         res.json({
