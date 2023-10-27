eca13a4d3ab0a784ec6f54bf12c3a51a06f14ad9
lex <savchenko.lex@gmail.com>
2023-10-27 19:56:32 +0300

    add owner to filter in getContactById query

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -2,9 +2,9 @@
 const Contact = require("../../models/contactSchema.js");
 
 const getContactById = async (req, res, next) => {
-   
+      const currentUserID = req.user._id;
       const { contactId } = req.params;
-      const contact = await Contact.findById(contactId);
+      const contact = await Contact.findOne({_id: contactId, owner: currentUserID});
       
       if (!contact) {
         const error = requestError(404);
