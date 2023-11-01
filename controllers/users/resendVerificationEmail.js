8732e688cf7930013366f3745f1a0f04376e712e
lex <savchenko.lex@gmail.com>
2023-11-01 08:16:59 +0200

    cleaning

--- controllers/users/resendVerificationEmail.js
+++ controllers/users/resendVerificationEmail.js
@@ -6,26 +6,23 @@ const requestError = require('../../utils/requestError.js');
 async function resendVerificationEmail (req, res, next){
     
     const {email} = req.body;
-    console.log("resend email: ", email);
+    
     if(email){
         const userExist = await User.findOne({email});
-        console.log("userExist: ", userExist);
+        
         if(userExist){
             if(userExist.verify){
                 throw requestError(400, {"message": "Verification has already been passed"});
             } else {
                 const {_id} = userExist;
                 const verificationToken = uuidv4();
-                console.log("verToken: ", verificationToken);
-                const userUpdated = await User.findByIdAndUpdate(
-                                            {_id}, 
+                
+                await User.findByIdAndUpdate({_id}, 
                                             {verificationToken}, 
                                             {new: true}
                                             );
-                console.log("updated: ", userUpdated);
 
                 await sendVerificationEmail(email, verificationToken);
-
 
                 res.json({"message":"Verification email sent"});
             }
